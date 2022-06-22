1e58ce8e27b98cdcbe41b97456cf5213ade2d51a
TeoLambert <teolambert74@gmail.com>
2022-06-22 18:58:58 +0200

    Structure et fichier test

---
+++ src/test/java/fr/gamedev/stats/PointCalculatorTest.java
@@ -0,0 +1,28 @@
+package fr.gamedev.stats;
+
+import junit.framework.TestCase;
+import org.junit.BeforeClass;
+
+import java.awt.*;
+
+public class PointCalculatorTest extends TestCase {
+
+    PointCalculator pc;
+
+    @BeforeClass
+    public void beforeClass(){
+        pc = PointCalculator.getInstance();
+    }
+
+    public void testAccumulatedFsc() {
+
+    }
+
+
+    public void testRepetitionFsc(){
+    }
+
+
+    public void testLinearFsc(){
+    }
+}
