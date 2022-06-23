a78729e582d39614ebd4961245286a3c489d2437
vfabiano <vfabiano@esgi.fr>
2022-06-23 10:54:49 +0200

    ajout des tests repetitions

--- src/test/java/fr/gamedev/stats/PointCalculatorTest.java
+++ src/test/java/fr/gamedev/stats/PointCalculatorTest.java
@@ -1,12 +1,11 @@
 package fr.gamedev.stats;
 
+import fr.gamedev.stats.fixedSizeCoeficient.FscRule;
 import junit.framework.TestCase;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import fr.gamedev.stats.fixedSizeCoeficient.FscRule;
-
-import java.awt.*;
 
 public class PointCalculatorTest extends TestCase {
 
@@ -18,8 +17,8 @@ public class PointCalculatorTest extends TestCase {
     	int dataSource = 0;
     	FscRule rules = FscRule.fromString("[linear with first Time bonus, rounded up (accumulated_points)] FSC[(500)(up)(accumulated_points-*)i-1]");
     	assertEquals(510, pc.fsc(dataSource, true, (short) 10, rules));
-    }	
-    
+    }
+
     @Test
     public void testLinearFscFirstTimeWithValue(){
     	 pc = PointCalculator.getInstance();
@@ -27,7 +26,7 @@ public class PointCalculatorTest extends TestCase {
     	FscRule rules = FscRule.fromString("[linear with first Time bonus, rounded up (accumulated_points)] FSC[(500)(up)(accumulated_points-*)i-1]");
     	assertEquals(510, pc.fsc(dataSource, true, (short) 10, rules));
     }
-    
+
     @Test
     public void testLinearFscWithValue(){
     	 pc = PointCalculator.getInstance();
@@ -35,6 +34,44 @@ public class PointCalculatorTest extends TestCase {
     	FscRule rules = FscRule.fromString("[linear with first Time bonus, rounded up (accumulated_points)] FSC[(500)(up)(accumulated_points-*)i-1]");
     	assertEquals(10, pc.fsc(dataSource, false, (short) 10, rules));
     }
-    
-    
+
+
+    public void testRepetitionFscFistTime(){
+        pc = PointCalculator.getInstance();
+        FscRule rule = FscRule.fromString("[(repetition)] FSC[(5)(down)(repetition-*)10-1|50-0.75|100-0.5|i-0.001]");
+
+        int expect_rst = 55;
+        int result = pc.fsc(0, true, (short)50, rule);
+        assertEquals(expect_rst, result);
+    }
+
+    public void testRepetitionFscSecondTime(){
+        pc = PointCalculator.getInstance();
+        FscRule rule = FscRule.fromString("[(repetition)] FSC[(5)(down)(repetition-*)10-1|50-0.75|100-0.5|i-0.001]");
+
+        int expect_rst = 50;
+        int result = pc.fsc(1, false, (short)50, rule);
+        assertEquals(expect_rst, result);
+    }
+
+    public void testRepetitionFscHundredTime(){
+        pc = PointCalculator.getInstance();
+        FscRule rule = FscRule.fromString("[(repetition)] FSC[(5)(down)(repetition-*)10-1|50-0.75|100-0.5|i-0.001]");
+
+        int expect_rst = 25;
+        int result = pc.fsc(99, false, (short)50, rule);
+        assertEquals(expect_rst, result);
+    }
+
+    public void testRepetitionFscTwoHundredFiftyTime(){
+        pc = PointCalculator.getInstance();
+        FscRule rule = FscRule.fromString("[(repetition)] FSC[(5)(down)(repetition-*)10-1|50-0.75|100-0.5|i-0.001]");
+
+        int expect_rst = 0;
+        int result = pc.fsc(249, false, (short)50, rule);
+        assertEquals(expect_rst, result);
+    }
+
+    public void testLinearFsc(){
+    }
 }
