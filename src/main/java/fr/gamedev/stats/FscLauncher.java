de2489ca85c09bf7eb6a397dc1a7079e69c29d57
teolamb <teolambert74@gmail.com>
2022-06-24 15:57:48 +0200

    API Ready

--- src/main/java/fr/gamedev/stats/FscLauncher.java
+++ src/main/java/fr/gamedev/stats/FscLauncher.java
@@ -1,7 +1,9 @@
 package fr.gamedev.stats;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class FscLauncher
 {
     public static void main(String[] args){
