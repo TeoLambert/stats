73594066e92eafe5a35bdb54f8b7e6e1e3b3ce26
Clément Guiton <clement.guiton.dev@gmail.com>
2022-06-24 16:10:04 +0200

    passage de rule en param

--- src/main/java/fr/gamedev/stats/controller/FscControler.java
+++ src/main/java/fr/gamedev/stats/controller/FscControler.java
@@ -13,7 +13,16 @@ public class FscControler {
 	@RequestMapping("/stats/fsc")
 	public int calculPointFsc(@RequestParam() int currentPoint, @RequestParam(defaultValue = "false") boolean isFirstTime, @RequestParam() short basePoints,@RequestParam() String ruleConfig) {
 		PointCalculator pc = PointCalculator.getInstance();
-    	FscRule rule = FscRule.fromString(ruleConfig);
+		FscRule rule
+		if(ruleConfig.contains("linear")){
+			rule = FscRule.fromString("[linear with first Time bonus, rounded up (accumulated_points)] FSC[(500)(up)(accumulated_points-*)i-1]");
+		}
+		else if (ruleConfig.contains("repetition")) {
+			rule = FscRule.fromString("[(repetition)] FSC[(5)(down)(repetition-*)10-1|50-0.75|100-0.5|i-0.001]");
+		}
+		else {
+			rule = FscRule.fromString("[Fixed step coefficient plus firstTime bonus rounded up (accumulated points)] FSC[(100)(up)(accumulated_points-*)500-2|1000-1.5|2000-1|5000-0.75|10000-0.5|50000-0.25|i-0.01]");
+		}
 		return pc.fsc(currentPoint, isFirstTime, basePoints, rule);
 	}
 
