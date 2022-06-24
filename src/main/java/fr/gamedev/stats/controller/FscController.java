63da36f1b0ea446b7983481aa949bd30c0da34b9
vfabiano <vfabiano@esgi.fr>
2022-06-24 17:48:17 +0200

    ajout rule fscController

--- src/main/java/fr/gamedev/stats/controller/FscController.java
+++ src/main/java/fr/gamedev/stats/controller/FscController.java
@@ -10,19 +10,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class FscController {
 
     static FscRule rule1 = FscRule.fromString( "[Accumulated Points : Fixed step coefficient plus firstTime bonus rounded up (accumulated points)] FSC[(100)(up)(accumulated_points-*)500-2|1000-1.5|2000-1|5000-0.75|10000-0.5|50000-0.25|i-0.01]");
-    static FscRule rule2 = FscRule.fromString( "[Test2] FSC[(10000)(up)(accumulated_points-*)500-2|1000-1.5|2000-1|5000-0.75|10000-0.5|50000-0.25|i-0.01]");
+    static FscRule rule2 = FscRule.fromString( "[linear with first Time bonus, rounded up (accumulated_points)] FSC[(500)(up)(accumulated_points-*)i-1]");
+    static FscRule rule3 = FscRule.fromString("[(repetition)] FSC[(5)(down)(repetition-*)10-1|50-0.75|100-0.5|i-0.001]");
 
     @RequestMapping("/stats/fsc")
     public int calculFsc(@RequestParam() int currentPoint, @RequestParam() boolean isFirstTime, @RequestParam() short nbPoints, @RequestParam() String ruleName) throws Exception {
 
         FscRule finalRule;
 
-        if(ruleName.equals("rule1"))
-            finalRule = rule1;
-        else if(ruleName.equals("rule2"))
-            finalRule = rule2;
-        else
-            throw new Exception("Rule name doesn't exist");
+        switch (ruleName) {
+            case "rule1":
+                finalRule = rule1;
+                break;
+            case "rule2":
+                finalRule = rule2;
+                break;
+            case "rule3":
+                finalRule = rule3;
+                break;
+            default:
+                throw new Exception("Rule name doesn't exist");
+        }
+
 
         return PointCalculator.getInstance().fsc(currentPoint, isFirstTime, nbPoints, finalRule);
     }
